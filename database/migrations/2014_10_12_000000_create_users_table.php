7c3af0942b303ee138b428de1ecb74e2252db3df
david00910 <david.antal211@gmail.com>
2019-11-04 15:51:07 +0100

    Basic auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('gender');
+            $table->string('phone');
+            $table->bigInteger('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
